b1a9a782a283b1c12ddc21f716c84264d3232ee7
Konstantin Atroschenko <kostay375298918971@gmail.com>
2025-02-17 10:03:27 +0300

    feat: Разделил сборку на dev и prod

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,24 +1,26 @@
 import webpack from "webpack";
 
 import { buildWebpackConfig } from "./config/build/buildWebpackConfig";
-import { BuildPath } from "./config/build/types/config";
+import { BuildEnv, BuildPath } from "./config/build/types/config";
 import path from "path";
 
-const paths: BuildPath = {
-  entry: path.resolve(__dirname, "src", "index.js"),
-  build: path.resolve(__dirname, "build"),
-  html: path.resolve(__dirname, "public", "index.html"),
+export default (env: BuildEnv) => {
+  const paths: BuildPath = {
+    entry: path.resolve(__dirname, "src", "index.js"),
+    build: path.resolve(__dirname, "build"),
+    html: path.resolve(__dirname, "public", "index.html"),
+  };
+
+  const mode = env.mode || "development";
+  const isDev = mode === "development";
+  const PORT = env.port || 3000;
+
+  const config: webpack.Configuration = buildWebpackConfig({
+    mode: "development",
+    paths,
+    isDev,
+    port: PORT,
+  });
+
+  return config;
 };
-
-const mode = "development";
-const isDev = mode === "development";
-const PORT = 3000;
-
-const config: webpack.Configuration = buildWebpackConfig({
-  mode: "development",
-  paths,
-  isDev,
-  port: PORT,
-});
-
-export default config;
