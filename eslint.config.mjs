126b016b79834980b9640245c838ca3ccb89a200
Konstantin Atroschenko <kostay375298918971@gmail.com>
2025-02-18 11:35:25 +0300

    feat: В ESLint добавил hooksPlugin
    Сам плагин eslint-plugin-react-hooks еще не обновился, возможно не будет
    работать

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,16 +3,18 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import react from "eslint-plugin-react";
+import hooksPlugin from "eslint-plugin-react-hooks";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    plugins: { react },
+    plugins: { react, "react-hooks": hooksPlugin },
     languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } },
     rules: {
       "react/jsx-uses-react": "error",
       "react/jsx-uses-vars": "error",
+      ...hooksPlugin.configs.recommended.rules,
     },
   },
   { languageOptions: { globals: globals.browser } },
