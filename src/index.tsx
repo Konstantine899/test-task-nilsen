a2778ab6eb676fafd40383e23ee8225c321d7b36
Konstantin Atroschenko <kostay375298918971@gmail.com>
2025-02-19 15:53:01 +0300

    feat: Подключил Redux Toolkit

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/App";
 import { BrowserRouter } from "react-router-dom";
+import { StoreProvider } from "app/providers/store-provider";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <StoreProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StoreProvider>
 );
