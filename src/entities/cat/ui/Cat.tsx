f39b3e17254a2b25c244ecc787f414dd242d5c4a
Konstantin Atroschenko <kostay375298918971@gmail.com>
2025-02-20 18:52:04 +0300

    feat: Добавил FavoriteIcon

--- src/entities/cat/ui/Cat.tsx
+++ src/entities/cat/ui/Cat.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { ICat } from "pages/cats/model/types/ICat";
 import { classNames } from "shared/lib/classNames";
 import * as styles from "./Cat.module.scss";
 import FavoriteBorderIcon from "shared/assets/favorite_border.svg";
+import FavoriteIcon from "shared/assets/favorite.svg";
 
 interface CatProps {
   cat: ICat;
@@ -10,11 +11,16 @@ interface CatProps {
 
 export const Cat = (props: CatProps) => {
   const { cat } = props;
+  const [showHeart, setShowHeart] = useState(false);
   return (
     <div className={classNames(styles.catItem)}>
       <img src={cat.url} alt={cat.id} />
-      <div className={styles.heartContainer}>
-        <FavoriteBorderIcon />
+      <div
+        className={styles.heartContainer}
+        onMouseEnter={() => setShowHeart(true)}
+        onMouseLeave={() => setShowHeart(false)}
+      >
+        {showHeart ? <FavoriteIcon /> : <FavoriteBorderIcon />}
       </div>
     </div>
   );
