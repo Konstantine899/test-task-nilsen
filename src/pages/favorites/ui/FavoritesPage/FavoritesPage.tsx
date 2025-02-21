b943f2b3cbc58fb934947219b77a7e8da6abbcbb
Konstantin Atroschenko <kostay375298918971@gmail.com>
2025-02-21 17:26:07 +0300

    feat: FavoritesPage
    Добавил состояние catsFromState в массив зависимостей useEffect

--- src/pages/favorites/ui/FavoritesPage/FavoritesPage.tsx
+++ src/pages/favorites/ui/FavoritesPage/FavoritesPage.tsx
@@ -3,13 +3,18 @@ import { classNames } from "shared/lib/classNames";
 import * as styles from "./FavoritesPage.module.scss";
 import { ICat } from "pages/cats/model/types/ICat";
 import FavoritesList from "pages/favorites/ui/FavoritesList/FavoritesList";
+import { useSelector } from "react-redux";
+import { RootState } from "app/providers/store-provider/config/store";
 
 const FavoritesPage = () => {
   const [favoritesCats, setFavoritesCats] = useState<ICat[]>([]);
+  const catsFromState = useSelector(
+    (state: RootState) => state.favorite.favoriteCats
+  );
 
   useEffect(() => {
     setFavoritesCats(JSON.parse(localStorage.getItem("favoriteCats")));
-  }, []);
+  }, [catsFromState]);
 
   return (
     <div className={classNames(styles["favorites-page"])}>
