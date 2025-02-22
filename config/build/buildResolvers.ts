c8d552b3e5375bec87bb7092bb1196997db66d3d
Konstantin Atroschenko <kostay375298918971@gmail.com>
2025-02-22 22:46:32 +0300

    refactor: buildResolvers добавил комментарии

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,11 +1,11 @@
-import { ResolveOptions } from "webpack";
-import { BuildOptions } from "./types/config";
+import {ResolveOptions} from "webpack";
+import {BuildOptions} from "./types/config";
+
 
 export function buildResolvers(options: BuildOptions): ResolveOptions {
   return {
-    extensions: [".tsx", ".ts", ".js"],
-    preferAbsolute: true,
-    modules: [options.paths.src, "node_modules"],
-    alias: {},
+    extensions: [".tsx", ".ts", ".js"], // Поддерживаемые расширения файлов
+    preferAbsolute: true, // Предпочтение абсолютных путей при разрешении модулей
+    modules: [options.paths.src, "node_modules"], // Порядок поиска модулей
   };
 }
