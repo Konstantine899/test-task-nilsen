d6a3e7656ac4f61d48f6010591d9a7f207d31e22
Konstantin Atroschenko <kostay375298918971@gmail.com>
2025-02-22 23:38:01 +0300

    feat: buildCssLoader
    Добавил поддержку esModule и sourseMap включаю только в режиме
    разработки

--- config/build/loaders/buildCssLoader.ts
+++ config/build/loaders/buildCssLoader.ts
@@ -2,25 +2,34 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import {BuildOptions} from "../types/config";
 import webpack from "webpack";
 
+/**
+ * Создает конфигурацию лоадеров для обработки CSS/SCSS-файлов.
+ *
+ * @param options - Опции сборки.
+ * @returns Конфигурация лоадера для CSS/SCSS.
+ */
+
 export function buildCssLoader(options: BuildOptions): webpack.RuleSetRule {
   const { isDev } = options;
   return {
-    test: /\.s[ac]ss$/i,
+    test: /\.s[ac]ss$/i, // Обработка SCSS/CSS-файлов
     use: [
-      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader, // Применение стилей в development или extraction в production
       {
-        loader: "css-loader",
+        loader: "css-loader", // Обработка CSS
         options: {
           modules: {
-            auto: (resourcePath: string) => resourcePath.includes(".module."),
+            auto: (resourcePath: string) =>
+                /\.(module)\.(s[ac]ss)$/.test(resourcePath), // Включать CSS Modules только для файлов .module.scss
             localIdentName: isDev
-              ? "[path][name]__[local]--[hash:base64:5]"
-              : "[hash:base64:8]",
+              ? "[path][name]__[local]--[hash:base64:5]" // Для разработки
+              : "[hash:base64:8]", // Для production
           },
-          sourceMap: true,
+          esModule: true, // Использовать ES Module экпорт (по умолчанию true)
+          sourceMap: isDev, // Включить source maps только для разработки
         },
       },
-      "sass-loader",
+      "sass-loader", // Компиляция SCSS в CSS
     ],
   };
 }
