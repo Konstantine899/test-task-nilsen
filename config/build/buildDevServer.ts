c8dd91f6d766683f38fcb26e52b2f437399a3bc2
Konstantin Atroschenko <kostay375298918971@gmail.com>
2025-02-22 23:07:05 +0300

    feat: buildDevServer добавил настроек

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -1,11 +1,24 @@
-import { BuildOptions } from "./types/config";
-import { Configuration } from "webpack-dev-server";
+import {BuildOptions} from "./types/config";
+import {Configuration} from "webpack-dev-server";
+import path from "path";
 
 export function buildDevServer(options: BuildOptions): Configuration {
   return {
-    port: options.port,
-    open: true,
-    hot: true,
-    historyApiFallback: true,
+    port: options.port, // Порт для запуска сервера
+    open: true, // Автоматическое открытие браузера
+    hot: true, // Включение горячей замены модулей (HMR)
+    historyApiFallback: true, // Поддержка SPA (Single Page Application)
+    static: {
+      directory: path.resolve(__dirname, "public"), // Директория с статическими файлами
+    },
+    devMiddleware: {
+      writeToDisk: false, // Не записывать файлы на диск
+    },
+    client: {
+      logging: options.isDev ? "info" : "none", // Уровень логирования
+      overlay: options.isDev, // Включать overlay только для разработки
+      progress: options.isDev, // Показывать прогресс сборки
+      reconnect: 10, // Попытка переподключения каждые 10 секунд
+    },
   };
 }
