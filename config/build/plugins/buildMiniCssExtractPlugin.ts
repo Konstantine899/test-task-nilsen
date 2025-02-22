cb483b821f7b9dd8b5d6c33dc391a39890692e04
Konstantin Atroschenko <kostay375298918971@gmail.com>
2025-02-22 23:19:04 +0300

    feat: MiniCssExtractPlugin
    Включил проверку порядка импортов

--- config/build/plugins/buildMiniCssExtractPlugin.ts
+++ config/build/plugins/buildMiniCssExtractPlugin.ts
@@ -2,7 +2,8 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 export function buildMiniCssExtractPlugin(): MiniCssExtractPlugin {
   return new MiniCssExtractPlugin({
-    filename: "css/[name].[contenthash:8].css",
-    chunkFilename: "css/[name].[contenthash:8].css",
+    filename: "css/[name].[contenthash:8].css", // Имя основных CSS-файлов
+    chunkFilename: "css/[name].[contenthash:8].css", // Имя CSS-файлов для динамических импортов
+    ignoreOrder: false, // Включаю проверку порядка импортов
   });
 }
