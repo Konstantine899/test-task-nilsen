477f3905229a409839326466e000f27a6cadc278
Konstantin Atroschenko <kostay375298918971@gmail.com>
2025-02-23 22:01:48 +0300

    docs: IStateSchema

--- src/app/providers/store-provider/config/IStateSchema.ts
+++ src/app/providers/store-provider/config/IStateSchema.ts
@@ -1,7 +1,27 @@
-import { ICatsSchema } from "pages/cats";
-import { IFavoriteSchema } from "features/favorite";
+/**
+ * Интерфейс IStateSchema описывает структуру глобального состояния Redux store.
+ *
+ * @interface IStateSchema
+ * @description
+ * Представляет полную схему состояния приложения, включая модули "котики" и "избранное".
+ */
+import {ICatsSchema} from "pages/cats";
+import {IFavoriteSchema} from "features/favorite";
 
 export interface IStateSchema {
+  /**
+   * Состояние модуля "котики".
+   * @type {ICatsSchema}
+   * @description
+   * Содержит информацию о списках котиков, состоянии загрузки, ошибках, пагинации и наличии дополнительных данных.
+   */
   cats: ICatsSchema;
+
+  /**
+   * Состояние модуля "избранное".
+   * @type {IFavoriteSchema}
+   * @description
+   * Содержит список избранных котиков и связанные с ними данные.
+   */
   favorite: IFavoriteSchema;
 }
