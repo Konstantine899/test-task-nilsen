367ba189845887d48f2ca9215ef9986d203f0278
Konstantin Atroschenko <kostay375298918971@gmail.com>
2025-02-23 15:34:28 +0300

    refactor: throttle
    Добавил описание и более подробно описал типы

--- src/shared/lib/throttle.ts
+++ src/shared/lib/throttle.ts
@@ -1,30 +1,41 @@
-type Func = (...args: any[]) => void;
+type Func<T extends any[]> = (...args: T) => void;
 
-export function throttle<T extends Func>(func: T, limit: number): T {
+/**
+ * Создает throttled-версию функции.
+ *
+ * @param func - Исходная функция.
+ * @param limit - Интервал между вызовами (в миллисекундах).
+ * @returns Throttled-функция.
+ */
+
+export function throttle<T extends any[]>(func: Func<T>, limit: number): Func<T> {
     let inThrottle: boolean = false;
-    let savedThis: any = null;
-    let savedArgs: any[] | null = null;
+    let savedThis: null | ThisType<Func<T>> = null;
+    let savedArgs: T | null = null;
 
-    const throttledFunc = function (this: any, ...args: any[]) {
+    const throttledFunc: Func<T> = function (this: ThisType<Func<T>>, ...args: T) {
         if (inThrottle) {
+            // Сохраняем контекст и аргументы
             savedThis = this;
             savedArgs = args;
             return;
         }
 
-        func.apply(this, args);
+        func.apply(this, args); // Вызываем оригинальную функцию
         inThrottle = true;
 
 
         setTimeout(() => {
             inThrottle = false;
+
+            // Если есть сохранённые аргументы, выполняем функцию с ними
             if (savedArgs) {
-                throttledFunc.apply(savedThis, savedArgs);
+                throttledFunc.apply(savedThis!, savedArgs!);
                 savedThis = null;
                 savedArgs = null;
             }
         }, limit);
     };
 
-    return throttledFunc as T;
+    return throttledFunc;
 }
