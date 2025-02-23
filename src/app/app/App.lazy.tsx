a40fc3f847c31f594e20ec59f0683d3cf9a7265c
Konstantin Atroschenko <kostay375298918971@gmail.com>
2025-02-23 22:30:28 +0300

    docs: AppAsync

--- src/app/app/App.lazy.tsx
+++ src/app/app/App.lazy.tsx
@@ -1,10 +1,34 @@
 import React, {lazy, Suspense} from "react";
 import PageLoader from "widgets/page-loader/PageLoader";
+import ErrorBoundary from "app/providers/error-boundary/ErrorBoundary";
+import {BrowserRouter} from "react-router-dom";
+import {StoreProvider} from "app/providers/store-provider";
+
+/**
+ * Компонент AppAsync представляет асинхронную версию основного приложения.
+ *
+ * @returns JSX-элемент, содержащий всю структуру приложения с поддержкой ленивой загрузки.
+ *
+ * @remarks
+ * Этот компонент оборачивает приложение в следующие провайдеры:
+ * - ErrorBoundary: Глобальная обработка ошибок.
+ * - StoreProvider: Провайдер Redux store.
+ * - BrowserRouter: Маршрутизация через React Router.
+ * Также используется React.lazy и Suspense для ленивой загрузки основного компонента App.
+ */
 
 const AppLazy = lazy(() => import("./App"));
 
 export const AppAsync = () => (
-    <Suspense fallback={<PageLoader />}>
-        <AppLazy />
-    </Suspense>
+    <ErrorBoundary> {/* Глобальная обработка ошибок */}
+        <StoreProvider> {/* Провайдер Redux store */}
+            <BrowserRouter> {/* Маршрутизация через React Router */}
+                <Suspense fallback={<PageLoader/>}> {/* Заглушка для ленивой загрузки */}
+                    <AppLazy/> {/* Ленивая загрузка основного приложения */}
+                </Suspense>
+            </BrowserRouter>
+        </StoreProvider>
+    </ErrorBoundary>
 );
+// Устанавливаем displayName для отладки
+AppAsync.displayName = "AppAsync";
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {App} from "./app/app";
-import {BrowserRouter} from "react-router-dom";
-import {StoreProvider} from "app/providers/store-provider";
-import ErrorBoundary from "app/providers/error-boundary/ErrorBoundary";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
-root.render(
-    <ErrorBoundary>
-        <StoreProvider>
-            <BrowserRouter>
-                <App/>
-            </BrowserRouter>
-        </StoreProvider>
-    </ErrorBoundary>
-);
+root.render(<App/>);
