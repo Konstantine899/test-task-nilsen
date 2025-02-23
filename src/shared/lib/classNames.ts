203258a65fbbfa4ecc494b00cba0d30d8bc80e16
Konstantin Atroschenko <kostay375298918971@gmail.com>
2025-02-23 15:13:15 +0300

    feat: classNames добавил описания

--- src/shared/lib/classNames.ts
+++ src/shared/lib/classNames.ts
@@ -1,14 +1,28 @@
+
+/**
+ * Создает строку классов CSS на основе базового класса, модификаторов и дополнительных классов.
+ *
+ * @param cls - Базовый класс.
+ * @param mods - Модификаторы классов (ключ — название класса, значение — условие активации).
+ * @param additional - Дополнительные классы (массив строк).
+ * @returns Строка с объединёнными классами.
+ */
+
 export type Mods = Record<string, boolean | string>;
+
 
 export function classNames(
   cls: string,
   mods: Mods = {},
   additional: Array<string | undefined> = []
 ): string {
-  const Mods: string[] = Object.entries(mods)
-    .filter(([_, value]) => Boolean(value))
-    .map(([className]) => className);
+  const modClasses: string[] = Object.entries(mods)
+    .filter(([_, value]) => Boolean(value)) // Оставляем только активные модификаторы
+    .map(([className]) => className); // Извлекаем их имена
 
+  // Фильтруем дополнительные классы от undefined
   const Additional = additional.filter(Boolean);
-  return [cls, ...Mods, ...Additional].join(" ");
+
+  // Объединяем все классы в одну строку
+  return [cls, ...modClasses, ...Additional].join(" ");
 }
