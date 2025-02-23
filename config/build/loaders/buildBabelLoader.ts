b0216539670ad47ef153423fa609e67069aad84e
Konstantin Atroschenko <kostay375298918971@gmail.com>
2025-02-23 09:39:47 +0300

    feat: buildBabelLoader

--- config/build/loaders/buildBabelLoader.ts
+++ config/build/loaders/buildBabelLoader.ts
@@ -5,15 +5,25 @@ interface BuildBabelLoaderProps extends BuildOptions {
   isTsx: boolean;
 }
 
+/**
+ * Создает конфигурацию лоадера Babel для транспиляции TypeScript/JavaScript.
+ *
+ * @param options - Опции сборки.
+ * @param options.isTsx - Флаг для обработки TSX-файлов (React).
+ * @returns Конфигурация лоадера Babel.
+ */
+
 export function buildBabelLoader(options: BuildBabelLoaderProps): webpack.RuleSetRule {
   const { isTsx } = options;
   return {
-    test: isTsx ? /\.(jsx|tsx)$/ : /\.(js|ts)$/,
-    exclude: /node_modules/,
+    test: isTsx
+        ? /\.(jsx|tsx)$/ // Поддержка .jsx, .tsx
+        : /\.(js|ts)$/, // Поддержка .js, .ts
+    exclude: /node_modules/, // Исключить node_modules
     use: {
       loader: "babel-loader",
       options: {
-        presets: ["@babel/preset-env"],
+        presets: ["@babel/preset-env"], // Базовые пресет
         plugins: [
           ["@babel/plugin-transform-typescript", { isTsx }],
           ["@babel/plugin-transform-runtime", {}],
