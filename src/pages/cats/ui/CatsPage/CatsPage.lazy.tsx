04110c2e6cdacb50b6e077002c918ec7aa261f76
Konstantin Atroschenko <kostay375298918971@gmail.com>
2025-02-23 17:16:04 +0300

    docs: CatsPageAsync

--- src/pages/cats/ui/CatsPage/CatsPage.lazy.tsx
+++ src/pages/cats/ui/CatsPage/CatsPage.lazy.tsx
@@ -1,5 +1,13 @@
-import React, { lazy, Suspense } from "react";
+import React, {lazy, Suspense} from "react";
 import PageLoader from "widgets/page-loader/PageLoader";
+
+/**
+ * Асинхронная версия компонента CatsPage.
+ *
+ * @remarks
+ * Использует React.lazy и Suspense для ленивой загрузки CatsPage.
+ * В качестве заглушки используется PageLoader.
+ */
 
 const CatsPageLazy = lazy(() => import("./CatsPage"));
 
