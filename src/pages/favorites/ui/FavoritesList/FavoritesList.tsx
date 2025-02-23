68fd311049cf7504c19ee4f5473785eceb1d3f63
Konstantin Atroschenko <kostay375298918971@gmail.com>
2025-02-23 20:10:47 +0300

    docs: FavoritesList

--- src/pages/favorites/ui/FavoritesList/FavoritesList.tsx
+++ src/pages/favorites/ui/FavoritesList/FavoritesList.tsx
@@ -1,16 +1,34 @@
 import React from "react";
-import { ICat } from "pages/cats/model/types/ICat";
-import { classNames } from "shared/lib/classNames";
-import { Cat } from "entities/cat";
+import {ICat} from "pages/cats/model/types/ICat";
+import {classNames} from "shared/lib/classNames";
+import {Cat} from "entities/cat";
 import * as styles from "./FavoritesList.module.scss";
 import NotFoundFavoriteCats from "pages/favorites/ui/NotFoundFavoriteCats/NotFoundFavoriteCats";
 
+/**
+ * Интерфейс пропсов для компонента FavoritesList.
+ */
 interface FavoritesListProps {
-  cats: ICat[];
+  cats: ICat[]; // Массив избранных котиков
 }
+
+/**
+ * Компонент FavoritesList отображает список избранных котиков.
+ *
+ * @param props.cats - Массив объектов ICat[], представляющих избранные котики.
+ * @returns JSX-элемент со списком избранных котиков или сообщением об отсутствии данных.
+ *
+ * @remarks
+ * Если массив cats пустой, отображается компонент NotFoundFavoriteCats.
+ * Для каждого котика создаётся компонент Cat.
+ */
 
 const FavoritesList = (props: FavoritesListProps) => {
   const { cats } = props;
+
+  /**
+   * Если список избранных котиков пуст, отображаем компонент NotFoundFavoriteCats.
+   */
 
   if (!cats.length) {
     return <NotFoundFavoriteCats />;
@@ -24,5 +42,8 @@ const FavoritesList = (props: FavoritesListProps) => {
     </div>
   );
 };
+
+// Устанавливаем displayName для отладки
+FavoritesList.displayName = "FavoritesList";
 
 export default FavoritesList;
