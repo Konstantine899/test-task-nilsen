223311f4a194145523d9889f24c03c699718db12
Konstantin Atroschenko <kostay375298918971@gmail.com>
2025-02-23 17:51:41 +0300

    docs: catsSlice

--- src/pages/cats/model/slice/catsSlice.ts
+++ src/pages/cats/model/slice/catsSlice.ts
@@ -4,6 +4,10 @@ import {fetchCats} from "pages/cats/model/api/fetchCats";
 import {ICatsSchema} from "pages/cats/model/types/ICatsSchema";
 import {ICat} from "pages/cats/model/types/ICat";
 
+/**
+ * Начальное состояние редюсера catsSlice.
+ */
+
 const initialState: ICatsSchema = {
   cats: [],
   isLoading: false,
@@ -13,9 +17,15 @@ const initialState: ICatsSchema = {
 };
 
 export const catsSlice = createSlice({
-  name: "catsSlice",
+  name: "catsSlice", // Имя редюсера
   initialState,
   reducers: {
+      /**
+       * Обновляет номер текущей страницы.
+       *
+       * @param state - Текущее состояние.
+       * @param action - Действие с новым номером страницы.
+       */
     setPage: (state, action: PayloadAction<number>) => {
       state.page = action.payload;
     },
@@ -23,23 +33,35 @@ export const catsSlice = createSlice({
   extraReducers: (builder) =>
     builder
       .addCase(fetchCats.pending, (state) => {
+          /**
+           * Устанавливает флаг загрузки и очищает ошибку при начале запроса.
+           */
         state.isLoading = true;
         state.error = null;
       })
       .addCase(fetchCats.fulfilled, (state, action: PayloadAction<ICat[]>) => {
+          /**
+           * Обрабатывает успешный результат запроса.
+           * Добавляет новые данные в массив cats и обновляет hasMore.
+           */
         state.isLoading = false;
         state.error = null;
         if (action.payload.length > 0) {
-          state.cats = [...state.cats, ...action.payload];
+          state.cats = [...state.cats, ...action.payload]; // Добавляем новые данные
         } else {
           state.hasMore = false; // если новых данных нет
         }
       })
         .addCase(fetchCats.rejected, (state, action: any) => {
+            /**
+             * Обрабатывает ошибку запроса.
+             * Устанавливает флаг загрузки в false и сохраняет сообщение об ошибке.
+             */
             state.isLoading = false;
             state.error = action.payload || "Произошла ошибка";
         })
 });
 
+// Экспортируем действия и редюсер
 export const { actions: catsAction } = catsSlice;
 export const { reducer: catsReducer } = catsSlice;
