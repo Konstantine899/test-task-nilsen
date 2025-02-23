a477066e8a6ec2c8eac56c406d5f0dc25c0db55b
Konstantin Atroschenko <kostay375298918971@gmail.com>
2025-02-23 15:47:30 +0300

    feat: Skeleton
    Преобразовал числовые значения в строки

--- src/shared/ui/Skeleton/Skeleton.tsx
+++ src/shared/ui/Skeleton/Skeleton.tsx
@@ -1,6 +1,16 @@
-import React, { CSSProperties, memo } from "react";
-import { classNames } from "shared/lib/classNames";
+import React, {CSSProperties, memo} from "react";
+import {classNames} from "shared/lib/classNames";
 import * as cls from "./Skeleton.module.scss";
+
+/**
+ * Компонент Skeleton используется для отображения заглушки во время загрузки данных.
+ *
+ * @param className - Дополнительный класс для стилизации.
+ * @param height - Высота компонента (строка или число).
+ * @param width - Ширина компонента (строка или число).
+ * @param borderRadius - Радиус скругления (строка или число).
+ * @returns JSX-элемент Skeleton.
+ */
 
 interface SkeletonProps {
   className?: string;
@@ -12,11 +22,17 @@ interface SkeletonProps {
 export const Skeleton: React.FC<SkeletonProps> = memo(
   (props: SkeletonProps) => {
     const { borderRadius, width, height, className, ...otherProps } = props;
-    const styles: CSSProperties = { width, height, borderRadius };
+
+      // Преобразуем числовые значения в строки
+    const styles: CSSProperties = {
+        width: typeof  width === 'number' ? `${width}px` : width,
+        height: typeof height === "number" ? `${height}px` : height,
+        borderRadius: typeof borderRadius === "number" ? `${borderRadius}px`: borderRadius
+    }
 
     return (
       <div
-        className={classNames(cls.skeleton, {}, [className])}
+        className={classNames(cls.skeleton, {}, [className])} // Модификаторы не используются
         style={styles}
         {...otherProps}
       />
@@ -24,4 +40,5 @@ export const Skeleton: React.FC<SkeletonProps> = memo(
   }
 );
 
+// displayName для отладки
 Skeleton.displayName = `Skeleton`;
