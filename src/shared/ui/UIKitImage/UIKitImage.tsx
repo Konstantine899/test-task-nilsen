40be742af348610c036cc9e70bdb6d2551d899da
Konstantin Atroschenko <kostay375298918971@gmail.com>
2025-02-23 16:09:23 +0300

    feat: UiKitImage
    Обработал несколько негативных сценариев в частности если пользователь
    ничего не передал в атрибут src а так же скрываю img во время загрузки

--- src/shared/ui/UIKitImage/UIKitImage.tsx
+++ src/shared/ui/UIKitImage/UIKitImage.tsx
@@ -1,52 +1,80 @@
-import React, {
-  ImgHTMLAttributes,
-  JSX,
-  memo,
-  useLayoutEffect,
-  useState,
-} from "react";
+import React, {ImgHTMLAttributes, JSX, memo, useLayoutEffect, useState,} from "react";
+import {classNames, Mods} from "shared/lib/classNames";
+import * as styles from './UIKitImage.module.scss'
+
+/**
+ * Компонент UiKitImage используется для отображения изображений с поддержкой заглушек и изображений по умолчанию.
+ *
+ * @param src - URL изображения.
+ * @param alt - Альтернативный текст для изображения.
+ * @param mainImage - Заглушка для отображения во время загрузки.
+ * @param spareImage - Изображение по умолчанию при ошибке загрузки.
+ * @param className - Дополнительный класс для стилизации.
+ * @returns JSX-элемент UiKitImage.
+ */
 
 interface ImageProps extends ImgHTMLAttributes<HTMLImageElement> {
-  src: string;
-  alt?: string;
-  mainImage?: JSX.Element;
-  spareImage?: JSX.Element;
-  className?: string;
+    src: string; // URL изображения
+    alt?: string; // Альтернативный текст
+    mainImage?: JSX.Element; // Заглушка для загрузки
+    spareImage?: JSX.Element; // Изображение по умолчанию
+    className?: string; // Дополнительный класс
 }
 
 export const UiKitImage: React.FC<ImageProps> = memo((props: ImageProps) => {
-  const {
-    src,
-    alt = "img",
-    mainImage,
-    spareImage,
-    className,
-    ...otherProps
-  } = props;
+    const {
+        src,
+        alt = "img",
+        mainImage,
+        spareImage,
+        className,
+        ...otherProps
+    } = props;
 
-  const [isLoading, setIsLoading] = useState(true);
-  const [hasError, setHasError] = useState(false);
+    const [isLoading, setIsLoading] = useState(true); // Флаг загрузки
+    const [hasError, setHasError] = useState(false); // Флаг ошибки
 
-  useLayoutEffect(() => {
-    const image = new Image();
-    image.src = src ?? "";
-    image.onload = () => {
-      setIsLoading(true);
-    };
-    image.onerror = () => {
-      setIsLoading(false);
-      setHasError(true);
-    };
-  }, [src]);
+    useLayoutEffect(() => {
+        if (!src) {
+            console.warn("Аттрибут src пуст");
+            setIsLoading(false);
+            setHasError(true);
+            return;
+        }
 
-  if (isLoading && mainImage) {
-    return mainImage; // Отображается skeleton
-  }
+        const image = new Image();
+        image.src = src ?? "";
 
-  if (hasError && spareImage) {
-    return spareImage; // Отображается картинка по умолчанию
-  }
-  return <img className={className} src={src} alt={alt} {...otherProps} />;
+
+        // Обработка успешной загрузки
+        image.onload = () => {
+            setIsLoading(false); // Загрузка завершена
+            setHasError(false); // Ошибка отсутствует
+        };
+        // Обработка ошибки загрузки
+        image.onerror = () => {
+            setIsLoading(false); // Загрузка завершена
+            setHasError(true); // Установить флаг ошибки
+        };
+    }, [src]);
+
+    // Если загружается и есть mainImage, показываем её
+    if (isLoading && mainImage) {
+        return mainImage; // Отображается skeleton
+    }
+
+    // Если произошла ошибка и есть spareImage, показываем её
+    if (hasError && spareImage) {
+        return spareImage; // Отображается картинка по умолчанию
+    }
+
+    const mods: Mods = {
+        [styles.hide]: isLoading || hasError
+    }
+
+    // Если всё хорошо, показываем img
+    return <img className={classNames('', mods, [className])} src={src} alt={alt} {...otherProps} />;
 });
 
+// Устанавливаем displayName для отладки
 UiKitImage.displayName = `UiKitImage`;
