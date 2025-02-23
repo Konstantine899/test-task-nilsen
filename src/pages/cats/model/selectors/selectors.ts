8406b616433ea42867191ec9b82a86e82d74df9d
Konstantin Atroschenko <kostay375298918971@gmail.com>
2025-02-23 17:59:38 +0300

    docs: cats selectors

--- src/pages/cats/model/selectors/selectors.ts
+++ src/pages/cats/model/selectors/selectors.ts
@@ -1,29 +1,65 @@
-import { RootState } from "app/providers/store-provider/config/store";
-import { createSelector } from "@reduxjs/toolkit";
+import {RootState} from "app/providers/store-provider/config/store";
+import {createSelector} from "@reduxjs/toolkit";
+
+/**
+ * Базовый селектор для получения состояния cats из RootState.
+ *
+ * @param state - Глобальное состояние приложения.
+ * @returns Состояние cats.
+ */
 
 const getCatsState = (state: RootState) => state.cats;
 
+/**
+ * Получает список котиков из состояния.
+ *
+ * @returns Массив объектов ICat[].
+ */
 export const getCats = createSelector(
   getCatsState,
   (getCatsState) => getCatsState.cats
 );
+
+/**
+ * Получает флаг загрузки из состояния.
+ *
+ * @returns boolean — true, если данные загружаются.
+ */
 
 export const getCatsLoading = createSelector(
   getCatsState,
   (catsState) => catsState.isLoading
 );
 
+/**
+ * Получает сообщение об ошибке из состояния.
+ *
+ * @returns string | null — сообщение об ошибке или null, если ошибок нет.
+ */
+
 export const getCatsError = createSelector(
   getCatsState,
   (catsState) => catsState.error
 );
+
+/**
+ * Получает флаг наличия дополнительных данных из состояния.
+ *
+ * @returns boolean — true, если есть ещё данные для загрузки.
+ */
 
 export const getCatsHasMore = createSelector(
   getCatsState,
   (catsState) => catsState.hasMore
 );
 
+/**
+ * Получает текущую страницу пагинации из состояния.
+ *
+ * @returns number — номер текущей страницы.
+ */
+
 export const getCatsPage = createSelector(
   getCatsState,
-  (catsState) => catsState.page
+  (catsState) => catsState.page || 1
 );
