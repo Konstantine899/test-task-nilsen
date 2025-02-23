f5450c96c44a6031d348a9eded3ed0c506051175
Konstantin Atroschenko <kostay375298918971@gmail.com>
2025-02-23 17:29:01 +0300

    docs: ICatsSchema

--- src/pages/cats/model/types/ICatsSchema.ts
+++ src/pages/cats/model/types/ICatsSchema.ts
@@ -1,4 +1,15 @@
-import { ICat } from "pages/cats/model/types/ICat";
+import {ICat} from "pages/cats/model/types/ICat";
+
+/**
+ * Схема состояния для страницы "Котики".
+ *
+ * @interface ICatsSchema
+ * @property {ICat[]} cats - Массив объектов котиков.
+ * @property {boolean} isLoading - Флаг загрузки данных.
+ * @property {null | string} error - Сообщение об ошибке или null, если ошибки нет.
+ * @property {number} page - Текущая страница пагинации.
+ * @property {boolean} hasMore - Есть ли ещё данные для загрузки.
+ */
 
 export interface ICatsSchema {
   cats: ICat[];
