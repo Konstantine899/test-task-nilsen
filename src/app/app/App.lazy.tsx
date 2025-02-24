222a011c97b3e31a2956792f55bf0ccc18b2921c
Konstantin Atroschenko <kostay375298918971@gmail.com>
2025-02-24 10:48:33 +0300

    refactor: HashRouter
    Добавил HashRouter так как на стороне gh-pages при обновлении страницы
    не подгружались чанки

--- src/app/app/App.lazy.tsx
+++ src/app/app/App.lazy.tsx
@@ -1,6 +1,6 @@
 import React, {lazy, Suspense} from "react";
 import PageLoader from "widgets/page-loader/PageLoader";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import ErrorBoundary from "../providers/error-boundary/ErrorBoundary";
 import {StoreProvider} from "../providers/store-provider/ui/StoreProvider";
 
@@ -19,16 +19,18 @@ import {StoreProvider} from "../providers/store-provider/ui/StoreProvider";
 
 const AppLazy = lazy(() => import("./App"));
 
-export const AppAsync = () => (
-    <ErrorBoundary> {/* Глобальная обработка ошибок */}
+export const AppAsync = () => {
+    console.log("__PUBLIC_PATH__",__PUBLIC_PATH__)
+
+    return (<ErrorBoundary> {/* Глобальная обработка ошибок */}
         <StoreProvider> {/* Провайдер Redux store */}
-            <BrowserRouter basename={__PUBLIC_PATH__}> {/* Маршрутизация через React Router */}
+            <HashRouter> {/* Маршрутизация через React Router */}
                 <Suspense fallback={<PageLoader/>}> {/* Заглушка для ленивой загрузки */}
                     <AppLazy/> {/* Ленивая загрузка основного приложения */}
                 </Suspense>
-            </BrowserRouter>
+            </HashRouter>
         </StoreProvider>
-    </ErrorBoundary>
-);
+    </ErrorBoundary>)
+};
 // Устанавливаем displayName для отладки
 AppAsync.displayName = "AppAsync";
